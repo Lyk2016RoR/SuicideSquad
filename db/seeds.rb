994ab50ef35e3b6f6be60aa82fd655f552250b05
GizemAldemir <aldemirgizem@gmail.com>
2016-08-18 17:02:19 +0300

    Add writer id to books show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,9 @@
 c1= Category.create(name: "Aşk")
 c2= Category.create(name: "Macera")
 c3= Category.create(name: "Bilim-Kurgu")
+w1= Writer.create(name: "Elif Şafak")
+w2= Writer.create(name: "Cahit Sıtkı Tarancı")
+w3= Writer.create(name: "Dostoyevski")
 
-b1 = Book.create(name:"Aşk", published_at:"10.09.2010", description:"dfsdfsd", publishing_house:"Can Yayınları", category_id:1)
+b1 = Book.create(name:"Aşk", published_at:"10.09.2010", description:"dfsdfsd", publishing_house:"Can Yayınları", category_id:1 , writer_id:1)
+b2 = Book.create(name:"Suç ve Ceza", published_at:"10.09.1978", description:"dfsdfsd", publishing_house:"Can Yayınları", category_id:1 , writer_id:1)
