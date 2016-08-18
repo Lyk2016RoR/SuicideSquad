32ee1dad8389eb9ad93de8aa65e357807ea3dd75
Fadime Özhan <fadime_ozhan@hotmail.com>
2016-08-18 20:49:21 +0300

    Fix Migrations

--- db/migrate/20160818163246_add_book_id_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddBookIdToComments < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :comments, :book, foreign_key: true
-  end
-end
--- db/migrate/20160818163335_add_writer_id_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddWriterIdToComments < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :comments, :writer, foreign_key: true
-  end
-end
