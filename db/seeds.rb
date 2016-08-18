a0701ba0ad2da7e5fb5d9ece07a240b79e998716
Fadime Özhan <fadime_ozhan@hotmail.com>
2016-08-18 16:40:50 +0300

    Add Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+c1= Category.create(name: "Aşk")
+c2= Category.create(name: "Macera")
+c3= Category.create(name: "Bilim-Kurgu")
+
+b1 = Book.create(name:"Aşk", published_at:"10.09.2010", description:"dfsdfsd", publishing_house:"Can Yayınları", category_id:1)
